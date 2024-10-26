646de066026faf15911d57b9b679930b9677d991
Eric Bieszczad-Stie <35773677+BeatsuDev@users.noreply.github.com>
2024-10-26 17:32:56 +0200

    feat: add borders to player control buttons

--- src/components/player/PlayerButton.vue
+++ src/components/player/PlayerButton.vue
@@ -1,6 +1,6 @@
 <template>
     <button
-        class="p-2 rounded-full duration-75 bg-gray-800 hover:bg-gray-700 text-gray-50"
+        class="p-2 rounded-full duration-75 bg-gray-800 hover:bg-gray-700 text-gray-50 border-[1px] border-gray-500 hover:border-gray-400"
     >
         <slot></slot>
     </button>
