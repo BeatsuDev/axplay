383375baa20c5a3324a50e479e1ea2deb35a31b5
Eric Bieszczad-Stie <35773677+BeatsuDev@users.noreply.github.com>
2024-10-26 10:01:54 +0200

    chore: add player stubs

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,23 @@
-<script setup lang="ts">
-import MainLayout from "./layouts/MainLayout.vue";
-import MusicCollectionPage from "./pages/MusicCollectionPage.vue";
-</script>
-
 <template>
     <div class="h-screen bg-gray-900 text-gray-50">
         <MainLayout>
             <template #aside> This is an aside </template>
             <template #main> <MusicCollectionPage /> </template>
             <template #footer>
-                <p>&copy; 2021</p>
+                <PlayerControls />
+                <PlayerProgress v-model="progress" max="20" />
             </template>
         </MainLayout>
     </div>
 </template>
+
+<script setup lang="ts">
+import MainLayout from "./layouts/MainLayout.vue";
+import MusicCollectionPage from "./pages/MusicCollectionPage.vue";
+import PlayerControls from "./components/PlayerControls.vue";
+import PlayerProgress from "./components/PlayerProgress.vue";
+
+import { ref } from "vue";
+
+const progress = ref<number>(15);
+</script>
---
+++ src/components/PlayerProgress.vue
@@ -0,0 +1,11 @@
+<template>
+    <progress class="w-full" :value="progress" :max="length ?? 0"></progress>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+    length?: number; // The length of the track in milliseconds
+}>();
+
+const progress = defineModel<number>({ default: 0 });
+</script>
