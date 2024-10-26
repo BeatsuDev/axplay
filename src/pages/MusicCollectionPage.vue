701b3128e7e4736d98e88f58ef41829a037d476a
Eric Bieszczad-Stie <35773677+BeatsuDev@users.noreply.github.com>
2024-10-27 01:16:17 +0200

    feat: select music from collection

--- src/pages/MusicCollectionPage.vue
+++ src/pages/MusicCollectionPage.vue
@@ -1,13 +1,39 @@
 <template>
-    <div class="flex justify-center align-center w-full h-full bg-gray-800">
-        <h1>Music Collection</h1>
-        <ul>
-            <li v-for="music in collection" :key="music.filePath">
-                <div>{{ music.title }}</div>
-                <div>{{ music.artists?.join(", ") ?? "No artist" }}</div>
-                <div>{{ music.album }}</div>
-            </li>
-        </ul>
+    <div class="flex flex-col align-center w-full h-full bg-gray-800">
+        <h1 class="text-3xl text-center p-4 text-primary-500">
+            Music Collection
+        </h1>
+        <table v-if="collection.length > 0">
+            <colgroup>
+                <col style="width: 20%" />
+                <col style="width: 40%" />
+                <col style="width: 35%" />
+                <col style="width: 5%" />
+            </colgroup>
+            <thead>
+                <tr class="[&>*]:text-start">
+                    <th>Artist</th>
+                    <th>Song</th>
+                    <th>Album</th>
+                    <th>Duration</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr
+                    v-for="music in collection"
+                    :key="music.filePath"
+                    @click="$emit('selected', music)"
+                    class="border-t-2 border-b-2 border-gray-700 hover:bg-gray-700 cursor-pointer [&>*]:p-2"
+                >
+                    <td class="text-sm text-gray-400">
+                        {{ music.artists?.join(", ") ?? "No artist" }}
+                    </td>
+                    <td class="text-start">{{ music.title }}</td>
+                    <td>{{ music.album }}</td>
+                    <td>{{ formatTime(music.duration) }}</td>
+                </tr>
+            </tbody>
+        </table>
     </div>
 </template>
 
@@ -16,5 +42,15 @@ import { type MusicData } from "@/composables/music";
 
 defineProps<{
     collection: MusicData[];
+}>();
+
+function formatTime(seconds: number) {
+    const minutes = Math.floor(seconds / 60);
+    const remainingSeconds = Math.floor(seconds % 60);
+    return `${minutes}:${remainingSeconds.toString().padStart(2, "0")}`;
+}
+
+defineEmits<{
+    (e: "selected", music: MusicData): void;
 }>();
 </script>
