09fab83334fb0e0cb6b8a035da76bb1a00dc3bf2
Eric Bieszczad-Stie <35773677+BeatsuDev@users.noreply.github.com>
2024-10-26 18:53:50 +0200

    feat: add working player progress component

--- src/components/player/PlayerProgress.vue
+++ src/components/player/PlayerProgress.vue
@@ -1,11 +1,57 @@
 <template>
-    <progress class="w-full" :value="progress" :max="length ?? 0"></progress>
+    <div
+        ref="container"
+        class="relative w-full leading-[0] p-2 cursor-pointer select-none"
+        @mousedown="startScrubbing"
+    >
+        <progress
+            class="w-full duration-150 appearance-none h-1 [&::-webkit-progress-value]:bg-primary-500 [&::-webkit-progress-bar]:bg-gray-500"
+            :value="progress"
+            :max="length ?? 0"
+        ></progress>
+    </div>
 </template>
 
 <script setup lang="ts">
-defineProps<{
+import { ref } from "vue";
+import { useElementBounding } from "@vueuse/core";
+
+const props = defineProps<{
     length?: number; // The length of the track in milliseconds
 }>();
 
 const progress = defineModel<number>({ default: 0 });
+
+// Scrubbing
+const emit = defineEmits<{
+    (e: "change", progress: number): void;
+}>();
+
+const container = ref<HTMLDivElement | null>(null);
+const { left, right } = useElementBounding(container);
+
+function startScrubbing(event: MouseEvent) {
+    window.addEventListener("mousemove", scrub);
+    window.addEventListener("mouseup", stopScrubbing);
+    scrub(event);
+}
+
+function stopScrubbing() {
+    window.removeEventListener("mousemove", scrub);
+    window.removeEventListener("mouseup", stopScrubbing);
+
+    emit("change", progress.value);
+}
+
+function scrub(event: MouseEvent) {
+    if (!container.value) return;
+
+    const x = event.clientX;
+    const leftOffset = Math.min(
+        1,
+        Math.max(0, (x - left.value) / (right.value - left.value))
+    );
+
+    progress.value = leftOffset * (props.length ?? 0);
+}
 </script>
